16609c99a8c4ce2bcaa2a143c740db51a488248a
Vitalii Makarchenkov <mail@vitaliimak.com>
2023-06-24 20:27:23 +0300

    Add speed

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,22 @@
 const canvas = document.getElementById("board");
+const score = document.getElementById("score");
+const speed = document.getElementById("speed");
 const ctx = canvas.getContext("2d");
 const SIZE = 25;
 const BOARD = canvas.width;
 
 let interval;
 let direction = "right";
+let intervalTime = 500;
 
 const apple = {
-  x: Math.floor(Math.random() * BOARD / SIZE) * SIZE,
-  y: Math.floor(Math.random() * BOARD / SIZE) * SIZE,
+  x: randomCoordinate(),
+  y: randomCoordinate(),
 }
 const snake = [{
   x: BOARD / 2,
   y: BOARD / 2,
 }];
-snake[1] = {
-  x: BOARD / 2 - SIZE,
-  y: BOARD / 2,
-}
-snake[2] = {
-  x: BOARD / 2 - 2 * SIZE,
-  y: BOARD / 2,
-}
-
 
 function main() {
   
@@ -30,23 +24,54 @@ function main() {
     changeDirection(e);
   })
   
-  // drawApple();
-  interval = setInterval(drawSnake, 500);
+  interval = setInterval(drawSnake, calculateTimeInterval(speed));
 }
 
+function calculateTimeInterval() {
+  return 1 / Number.parseInt(speed.innerText) * 500;
+}
+
+function calculateSpeed(timeout) {
+  return 1 / timeout * 500;
+}
 function changeDirection(event) {
   if (event.keyCode === 39) {
+    if (direction === "left") {
+      console.error("Error!");
+      clearInterval(interval);
+      return;
+    }
     direction = "right";
   } else if (event.keyCode === 37) {
+    if (direction === "right") {
+      console.error("Error!");
+      clearInterval(interval);
+      return;
+    }
     direction = "left";
   } else if (event.keyCode === 38) {
+    if (direction === "down") {
+      console.error("Error!");
+      clearInterval(interval);
+      return;
+    }
     direction = "up";
   } else if (event.keyCode === 40) {
+    if (direction === "up") {
+      console.error("Error!");
+      clearInterval(interval);
+      return;
+    }
     direction = "down";
   }
 }
 
 function collision(box, limit) {
+  // for (let i = 1; i < snake.length; i++) {
+  //   if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+  //     return true;
+  //   }
+  // }
   return (box.x < 0 || box.y < 0) || (box.x + SIZE > limit || box.y + SIZE > limit);
 }
 
@@ -74,32 +99,31 @@ function drawSnake() {
       } else if (direction === "down") {
         snake[i].y += SIZE;
       }
-
+      eatApple(apple, snake[0]);
     }
     
-  // }
-  // for (const box of snake) {
-      
-    // ctx.save();
-    
-    // drawApple();
+    drawApple();
     
     if (collision(snake[i], BOARD)) {
       console.error("Error!");
       clearInterval(interval);
       return;
     }
-    // ctx.restore();
   }
   snake.forEach(box => drawSnakeBox(box));
-  // eatApple(apple, snake[0]);
 }
 
 function eatApple(apple, snakeBox) {
   if (apple.x === snakeBox.x && apple.y === snakeBox.y) {
-    snake.unshift(apple);
+    score.innerText = Number.parseInt(score.innerText) + 1;
+    snake.push({x: apple.x, y: apple.y});
     moveApple();
-    console.log(apple, snake);
+    intervalTime = calculateTimeInterval();
+    if (score.innerText % 5 === 0 && intervalTime > 70) {
+      speed.innerText = Number.parseInt(speed.innerText) + 1;
+      clearInterval(interval);
+      interval = setInterval(drawSnake, intervalTime);
+    }
   }
 }
 
@@ -109,10 +133,13 @@ function drawApple() {
 }
 
 function moveApple() {
-  apple.x = Math.floor(Math.random() * BOARD / SIZE) * SIZE;
-  apple.y = Math.floor(Math.random() * BOARD / SIZE) * SIZE;
+  apple.x = randomCoordinate();
+  apple.y = randomCoordinate();
 }
 
+function randomCoordinate() {
+  return Math.floor(Math.random() * BOARD / SIZE) * SIZE;
+}
 
 function drawSnakeBox(box) {
   ctx.fillStyle = "green";
